fa281a2ef1f57c903ca87edc4439bcbef2d48c7c
Adam Kadlec <adam.kadlec@gmail.com>
2022-01-17 14:17:46 +0100

    Updating ws controller

--- src/Controllers/ExchangeController.php
+++ src/Controllers/ExchangeController.php
@@ -15,13 +15,18 @@
 
 namespace FastyBird\MiniServer\Controllers;
 
+use FastyBird\DevicesModule\Entities as DevicesModuleEntities;
+use FastyBird\DevicesModule\Models as DevicesModuleModels;
+use FastyBird\DevicesModule\Queries as DevicesModuleQueries;
 use FastyBird\Exchange\Publisher as ExchangePublisher;
 use FastyBird\Metadata;
 use FastyBird\Metadata\Exceptions as MetadataExceptions;
+use FastyBird\Metadata\Helpers as MetadataHelpers;
 use FastyBird\Metadata\Loaders as MetadataLoaders;
 use FastyBird\Metadata\Schemas as MetadataSchemas;
 use FastyBird\MiniServer\Exceptions;
 use IPub\WebSockets;
+use IPub\WebSocketsWAMP;
 use Nette\Utils;
 use Psr\Log;
 use Throwable;
@@ -37,6 +42,18 @@ use Throwable;
 final class ExchangeController extends WebSockets\Application\Controller\Controller
 {
 
+	/** @var DevicesModuleModels\Devices\Properties\IPropertyRepository */
+	private DevicesModuleModels\Devices\Properties\IPropertyRepository $devicePropertiesRepository;
+
+	/** @var DevicesModuleModels\Channels\Properties\IPropertyRepository */
+	private DevicesModuleModels\Channels\Properties\IPropertyRepository $channelPropertiesRepository;
+
+	/** @var DevicesModuleModels\States\IDevicePropertyRepository */
+	private DevicesModuleModels\States\IDevicePropertyRepository $devicePropertiesStatesRepository;
+
+	/** @var DevicesModuleModels\States\IChannelPropertyRepository */
+	private DevicesModuleModels\States\IChannelPropertyRepository $channelPropertiesStatesRepository;
+
 	/** @var ExchangePublisher\IPublisher|null */
 	private ?ExchangePublisher\IPublisher $publisher;
 
@@ -50,6 +67,10 @@ final class ExchangeController extends WebSockets\Application\Controller\Control
 	private Log\LoggerInterface $logger;
 
 	public function __construct(
+		DevicesModuleModels\Devices\Properties\IPropertyRepository $devicePropertiesRepository,
+		DevicesModuleModels\Channels\Properties\IPropertyRepository $channelPropertiesRepository,
+		DevicesModuleModels\States\IDevicePropertyRepository $devicePropertiesStatesRepository,
+		DevicesModuleModels\States\IChannelPropertyRepository $channelPropertiesStatesRepository,
 		MetadataLoaders\ISchemaLoader $schemaLoader,
 		MetadataSchemas\IValidator $jsonValidator,
 		?ExchangePublisher\IPublisher $publisher = null,
@@ -57,10 +78,96 @@ final class ExchangeController extends WebSockets\Application\Controller\Control
 	) {
 		parent::__construct();
 
+		$this->devicePropertiesRepository = $devicePropertiesRepository;
+		$this->channelPropertiesRepository = $channelPropertiesRepository;
+		$this->devicePropertiesStatesRepository = $devicePropertiesStatesRepository;
+		$this->channelPropertiesStatesRepository = $channelPropertiesStatesRepository;
+
 		$this->schemaLoader = $schemaLoader;
 		$this->publisher = $publisher;
 		$this->jsonValidator = $jsonValidator;
 		$this->logger = $logger ?? new Log\NullLogger();
+	}
+
+	/**
+	 * @param WebSocketsWAMP\Entities\Clients\IClient $client
+	 * @param WebSocketsWAMP\Entities\Topics\ITopic $topic
+	 *
+	 * @return void
+	 *
+	 * @throws Utils\JsonException
+	 */
+	public function actionSubscribe(
+		WebSocketsWAMP\Entities\Clients\IClient $client,
+		WebSocketsWAMP\Entities\Topics\ITopic $topic
+	): void {
+		$findDevicesProperties = new DevicesModuleQueries\FindDevicePropertiesQuery();
+
+		$devicesProperties = $this->devicePropertiesRepository->getResultSet($findDevicesProperties);
+
+		/** @var DevicesModuleEntities\Devices\Properties\Property $deviceProperty */
+		foreach ($devicesProperties as $deviceProperty) {
+			$dynamicPropertyData = [];
+
+			if ($deviceProperty instanceof DevicesModuleEntities\Devices\Properties\IDynamicProperty) {
+				$devicePropertyState = $this->devicePropertiesStatesRepository->findOne($deviceProperty);
+
+				if ($devicePropertyState !== null) {
+					$dynamicPropertyData = [
+						'actual_value'   => $deviceProperty->getDataType() !== null ? MetadataHelpers\ValueHelper::normalizeValue($deviceProperty->getDataType(), $devicePropertyState->getActualValue(), $deviceProperty->getFormat()) : $devicePropertyState->getActualValue(),
+						'expected_value' => $deviceProperty->getDataType() !== null ? MetadataHelpers\ValueHelper::normalizeValue($deviceProperty->getDataType(), $devicePropertyState->getExpectedValue(), $deviceProperty->getFormat()) : $devicePropertyState->getExpectedValue(),
+						'pending'        => $devicePropertyState->isPending(),
+					];
+				}
+			}
+
+			$client->send(Utils\Json::encode([
+				WebSocketsWAMP\Application\Application::MSG_EVENT,
+				$topic->getId(),
+				Utils\Json::encode([
+					'routing_key' => Metadata\Types\RoutingKeyType::ROUTE_DEVICES_PROPERTY_ENTITY_REPORTED,
+					'origin'      => Metadata\Types\ModuleOriginType::ORIGIN_MODULE_DEVICES,
+					'data'        => array_merge(
+						$deviceProperty->toArray(),
+						$dynamicPropertyData,
+					),
+				]),
+			]));
+		}
+
+		$findChannelsProperties = new DevicesModuleQueries\FindChannelPropertiesQuery();
+
+		$channelsProperties = $this->channelPropertiesRepository->getResultSet($findChannelsProperties);
+
+		/** @var DevicesModuleEntities\Channels\Properties\Property $channelProperty */
+		foreach ($channelsProperties as $channelProperty) {
+			$dynamicPropertyData = [];
+
+			if ($channelProperty instanceof DevicesModuleEntities\Channels\Properties\IDynamicProperty) {
+				$channelPropertyState = $this->channelPropertiesStatesRepository->findOne($channelProperty);
+
+				if ($channelPropertyState !== null) {
+					$dynamicPropertyData = [
+						'actual_value'   => $channelProperty->getDataType() !== null ? MetadataHelpers\ValueHelper::normalizeValue($channelProperty->getDataType(), $channelPropertyState->getActualValue(), $channelProperty->getFormat()) : $channelPropertyState->getActualValue(),
+						'expected_value' => $channelProperty->getDataType() !== null ? MetadataHelpers\ValueHelper::normalizeValue($channelProperty->getDataType(), $channelPropertyState->getExpectedValue(), $channelProperty->getFormat()) : $channelPropertyState->getExpectedValue(),
+						'pending'        => $channelPropertyState->isPending(),
+					];
+				}
+			}
+
+			$client->send(Utils\Json::encode([
+				WebSocketsWAMP\Application\Application::MSG_EVENT,
+				$topic->getId(),
+				Utils\Json::encode([
+					'routing_key' => Metadata\Types\RoutingKeyType::ROUTE_CHANNELS_PROPERTY_ENTITY_REPORTED,
+					'origin'      => Metadata\Types\ModuleOriginType::ORIGIN_MODULE_DEVICES,
+					'data'        => array_merge(
+						$channelProperty->toArray(),
+						$dynamicPropertyData,
+					),
+				]),
+			]));
+		}
 	}
 
 	/**
@@ -86,8 +193,8 @@ final class ExchangeController extends WebSockets\Application\Controller\Control
 			case Metadata\Constants::MESSAGE_BUS_CHANNEL_PROPERTY_ACTION_ROUTING_KEY:
 			case Metadata\Constants::MESSAGE_BUS_CHANNEL_CONFIGURATION_ACTION_ROUTING_KEY:
 			case Metadata\Constants::MESSAGE_BUS_TRIGGER_ACTION_ROUTING_KEY:
-				$schema = $this->schemaLoader->loadByRoutingKey($args['routing_key']);
-				$data = $this->parseData($args, $schema);
+				$schema = $this->schemaLoader->loadByRoutingKey(Metadata\Types\RoutingKeyType::get($args['routing_key']));
+				$data = isset($args['data']) ? $this->parseData($args['data'], $schema) : null;
 
 				if ($this->publisher !== null) {
 					$this->publisher->publish(
