859a2ce66e0f1dfcfb37cbac452bc4100be5099f
Adam Kadlec <adam.kadlec@gmail.com>
2022-12-06 09:54:23 +0100

    Fixing tests

--- tools/phpunit-bootstrap.php
+++ tools/phpunit-bootstrap.php
@@ -3,7 +3,6 @@
 // phpcs:ignoreFile
 
 define('FB_APP_DIR', realpath(__DIR__ . '/..'));
-define('FB_CONFIG_DIR', FB_APP_DIR . DIRECTORY_SEPARATOR . 'var' . DIRECTORY_SEPARATOR . 'config');
 define('FB_VENDOR_DIR', realpath(__DIR__ . '/../vendor'));
 is_string(getenv('TEST_TOKEN'))
 	? define('FB_TEMP_DIR', __DIR__ . '/../var/tools/PHPUnit/tmp/' . getmypid() . '-' . getenv('TEST_TOKEN') ?? '')
